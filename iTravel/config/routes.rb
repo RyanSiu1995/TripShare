d6dacb90b073102255a3bca8fef7e8b8ef32adc3
shekodn <sdiazn19@gmail.com>
2018-02-06 19:12:00 +0800

    Add a Static Pages controller

--- iTravel/config/routes.rb
+++ iTravel/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#index'
+
 
 end
