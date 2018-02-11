fc1ce5de7b235a47a228579d986f5650d16d4ccc
shekodn <sdiazn19@gmail.com>
2018-02-11 15:46:01 +0800

    adds user validations (no devise yet)

--- iTravel/config/routes.rb
+++ iTravel/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
   resources :users
+  resources :static_pages
+
 
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/my_trips'
-  get 'static_pages/explore'
+
+  get    '/home',    to: 'static_pages#home'
+  get    '/my_trips',    to: 'static_pages#my_trips'
+  get    '/explore',    to: 'static_pages#explore'
+  
 
 end
