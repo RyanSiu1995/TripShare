2f63ad36c5337aa7869b0495f3b649d1bb8fefa3
shekodn <sdiazn19@gmail.com>
2018-02-11 15:10:07 +0800

    adds routes

--- iTravel/config/routes.rb
+++ iTravel/config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
 
   resources :users
 
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/my_trips'
   get 'static_pages/explore'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
-
 
 end
