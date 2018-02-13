0178fb4eabeee7507c7650ba480135febf726eae
shekodn <sdiazn19@gmail.com>
2018-02-13 16:18:14 +0800

    fixes name issue

--- iTravel/app/controllers/application_controller.rb
+++ iTravel/app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def hello
-  render html: "hello, world!"
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
 end
