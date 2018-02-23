02b88303493877957ac0d2aaeb2b9a70618b3084
shekodn <sdiazn19@gmail.com>
2018-02-23 19:18:45 +0800

    overrides colors

--- iTravel/app/assets/javascripts/application.js
+++ iTravel/app/assets/javascripts/application.js
@@ -28,3 +28,16 @@ $('.datepicker').pickadate({
   close: 'Ok',
   closeOnSelect: false // Close upon selecting a date,
 });
+
+$('input.autocomplete').autocomplete({
+  data: {
+    "Apple": null,
+    "Microsoft": null,
+    "Google": 'https://placehold.it/250x250'
+  },
+  limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+  onAutocomplete: function(val) {
+    // Callback function when value is autcompleted.
+  },
+  minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+});
