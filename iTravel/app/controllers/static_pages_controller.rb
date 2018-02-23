3bc53bdeb411d0843ae7703e1b503b3adec7e925
shekodn <sdiazn19@gmail.com>
2018-02-23 12:13:23 +0800

    add cached likes

--- iTravel/app/controllers/static_pages_controller.rb
+++ iTravel/app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    number_of_trips =  Trip.all.size
+    top_number = 10
+    @trips = Trip.all.sort_by(&:cached_weighted_score).reverse
+
   end
 
   def my_trips
