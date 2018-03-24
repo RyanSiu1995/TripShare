60b481297ac6b5de3f648081099af3babff82f4d
shekodn <sdiazn19@gmail.com>
2018-03-25 00:58:39 +0800

    adds comments

--- iTravel/app/controllers/comments_controller.rb
+++ iTravel/app/controllers/comments_controller.rb
@@ -21,7 +21,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @trip, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @city }
       else
-        format.html { render :new }
+        format.html { redirect_to @trip, notice: "Comment cannot be blank" }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
